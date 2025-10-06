c3e9755aaaca73bd754f146eb491914e7784cc2b
longern <771815005@qq.com>
2025-10-06 11:47:41 +0800

    Add MDX

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -5,4 +6,9 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
